743890844745f8caae4781d7eec3bda65c3eee42
Chaitanya-32 <t.chaitusai@gmail.com>
2019-07-24 20:20:19 +0530

    2 files modified, 18 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipeComponent } from './receipe/receipe.component';
+import { ReceipeListComponent } from './receipe/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './receipe/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './receipe/receipe-list/receipe-item/receipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, ReceipeComponent, ReceipeListComponent, ReceipeDetailComponent, ReceipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
